14d75c225fdd72ca2cb5bf75ec2e7ea2b7ba721b
Mohammad Danish <thatsdanish02@gmail.com>
2025-07-31 16:28:29 +0530

    Create subarrays.java

---
+++ Arrays/subarrays.java
@@ -0,0 +1,18 @@
+public class subarrays{
+    public static int subArrays(int numbers[]){
+
+        for(int i=0;i<numbers.length;i++){
+
+            int crt= numbers[i];
+            for(int j=0;j<nuumbers.length)
+            
+        }
+        return -1;
+
+    
+    }
+
+    public static void main(String[] args){
+        int  numbers[] = {2,4,6,8,10,12};
+    }
+}
