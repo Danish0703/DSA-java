7bc9e0955735283834fafd3ae56c48645e279617
Mohammad Danish <thatsdanish02@gmail.com>
2025-08-05 05:12:08 +0530

    sorting techniques

--- Arrays/sorting/sorting.java
+++ Arrays/sorting/sorting.java
@@ -1,5 +1,5 @@
-
-public class bubble {
+import java.util.*;
+public class sorting {
 
     public static void bubbleSort(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
@@ -29,7 +29,7 @@ public class bubble {
 
     }
 
-    public static void insrtionSort(int[] arr) {
+    public static void insertionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int curr = arr[i];
             int prev = i - 1;
@@ -40,8 +40,29 @@ public class bubble {
         }
     }
 
+    public static void quickSort(int[] arr){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest,arr[i]);
+        }
+        int count[]=new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //sorting
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = {20, 50, 60, 78, 90, 100};
+        int arr[] = {1,2,9,6,6,4,4,7,3,2};
         bubbleSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -51,7 +72,13 @@ public class bubble {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
         insertionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        quickSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
