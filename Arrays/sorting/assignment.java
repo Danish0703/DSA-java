583f230580786f71dbe38890b7246ac9246fff6f
Mohammad Danish <thatsdanish02@gmail.com>
2025-08-05 06:44:58 +0530

    Create assignment.java

---
+++ Arrays/sorting/assignment.java
@@ -0,0 +1,88 @@
+import java.util.*;
+public class sorting {
+
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[j] > arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i + 1;
+            while (prev <= 0 && arr[prev] < curr) {
+                arr[prev - 1] = arr[prev];
+                prev++;
+            }
+        }
+    }
+
+    public static void quickSort(int[] arr){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest,arr[i]);
+        }
+        int count[]=new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //sorting
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]<0){
+                arr[j]=i;
+                j--;
+                count[i]++;
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3,6,2,1,8,7,4,5,3,1};
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        insertionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        quickSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
+
